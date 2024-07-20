c06dc5865f337aa791e9833c3de7a983d1aa5997
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-07-20 19:58:24 +0530

    added first changes

---
+++ src/components/About.tsx
@@ -0,0 +1,27 @@
+
+
+export function About() {
+    return (
+    <div className="grid grid-cols-2 justify-between">
+        <div>
+            <div className="font-black text-6xl pb-5">
+                Neel
+            </div>
+            <div className="text-2xl font-mono">
+                Web3 Solana.
+            </div>
+            <div className="text-2xl font-mono">
+                Full Stack Developer.
+            </div>
+            <div className="flex text-2xl font-mono">
+                thinking about food <div className="pl-5 text-pink-600">❤</div>
+            </div>
+        </div>
+        <div className="w-40 h-40">
+            <img src="shin.jpeg"  />
+        </div>
+        <div className="font-semibold text-xl">Hey, i'm Neel i'm 20 year old solana & full stack developer based in india. i also love contributing to open source.</div>
+    </div>
+    )
+}
+
---
+++ src/components/AppBar.tsx
@@ -0,0 +1,15 @@
+
+
+export function AppBar() {
+    return <div className="flex justify-around mt-5 font-black backdrop-blur-xl rounded-3xl">
+        <button>
+            /
+        </button>
+        <button>
+            Tech Stack
+        </button>
+        <button>
+            Projects
+        </button>
+    </div>
+}
